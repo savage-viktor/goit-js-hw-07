39840481ea7e45af7fc150063cb1f49b27e32b9f
savage-viktor <viktor.dkn@gmail.com>
2022-05-10 23:33:30 +0300

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDivRef = document.querySelector(".gallery")
+
+galleryDivRef.innerHTML = galleryItems.map(({ preview, original, description }) => {
+
+    return `<div class="gallery__item" >
+        <a class="gallery__link" >
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+</div >`
+}).join("")
+
+function openModalBigImg(event) {
+    if (event.target === galleryDivRef) {
+        return
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+    instance.show()
+
+}
+
+galleryDivRef.addEventListener("click", openModalBigImg)
